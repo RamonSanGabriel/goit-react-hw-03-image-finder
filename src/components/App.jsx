d2728ef3140b69e870cd9ca32f492e50c17f17be
Ramon San Gabriel <jeshero11@gmail.com>
2024-08-18 13:15:54 +0800

    toaster

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import styles from './App.module.css';
 import { Searchbar } from './Searchbar/Searchbar';
 import { getAPI } from 'pixabay-api';
+import toast, { Toaster } from 'react-hot-toast';
 
 export class App extends Component {
   state = {
@@ -24,6 +25,25 @@ export class App extends Component {
       this.setState({ isLoading: true });
 
       const fetchedImages = await getAPI(search, page);
+
+      const { hits, totalHits } = fetchedImages;
+
+      if (search === '') return;
+
+      if (hits.length === 0) {
+        toast.error(
+          'Sorry there are no images matching your search query. Please try again.'
+        );
+        this.setState({ isLoading: false });
+        return;
+      }
+      if (page === 1) {
+        toast.success(`Hooray! we found ${totalHits} images!`);
+      }
+      if (page * 12 >= totalHits) {
+        this.setState({ isEnd: true });
+        toast("We're sorry, but you've reached the end of search results");
+      }
     } catch (error) {
     } finally {
     }
@@ -47,6 +67,7 @@ export class App extends Component {
     return (
       <div className={styles.App}>
         <Searchbar onSubmit={this.handleSubmit} />
+        <Toaster position="bottom-right" reverseOrder={false} />
       </div>
     );
   }
